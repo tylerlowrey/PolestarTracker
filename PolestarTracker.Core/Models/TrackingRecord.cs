5545696c4b5f4b56f1562db0241d4eb530a71305
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 15:59:31 -0400

    Added data object for tracking to be used w/ entity framework

---
+++ PolestarTracker.Core/Models/TrackingRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolestarTracker.Core.Models
+{
+    class TrackingRecord
+    {
+        public int RecordId { get; set; }
+        public string ProcessName { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
