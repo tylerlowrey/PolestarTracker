703a55cca68afb554c63739b4cd52f15de73f9e7
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 16:56:28 -0400

    Added test code for TrackingDataContext

--- PolestarTracker.Tests/EntityFramework/TrackingDataContextTests.cs
+++ PolestarTracker.Tests/EntityFramework/TrackingDataContextTests.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PolestarTracker.Core.Models;
 using PolestarTracker.EntityFramework;
 
 namespace PolestarTracker.Tests.EntityFramework
 {
     [TestClass]
-    class TrackingDataContextTests
+    public class TrackingDataContextTests
     {
+        [TestMethod]
+        public void Can_Successfully_Add_TrackingRecord_To_Database()
+        {
+            using (var db = new TrackingDataContext())
+            {
+                TrackingRecord testRecord = new TrackingRecord { ProcessName = "example process", Timestamp = DateTime.Now};
+                db.Add(testRecord);
+                db.SaveChanges();
+
+                var insertedRecord = db.TrackingRecords
+                    .OrderByDescending(tr => tr.RecordId)
+                    .FirstOrDefault();
+
+                Assert.AreEqual(testRecord, insertedRecord);
+            }
+        }
     }
 }
