6066c1cc0f8825e8ebea3b3743e5c18977a4b9f9
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 14:23:09 -0400

    Refactored common code into separate method GetActiveProcess

--- PolestarTracker.Core/ProcessTracker.cs
+++ PolestarTracker.Core/ProcessTracker.cs
@@ -18,20 +18,11 @@ namespace PolestarTracker.Core
         static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
         /**
-         * Original Poster: Jorge Ferreira
-         * Taken from: https://stackoverflow.com/questions/115868/how-do-i-get-the-title-of-the-current-active-window-using-c
+         * Adapted from: https://stackoverflow.com/questions/17345202/get-the-current-active-application-name
          */
         public static string GetActiveWindowTitle()
         {
-            const int nChars = 256;
-            StringBuilder Buff = new StringBuilder(nChars);
-            IntPtr handle = GetForegroundWindow();
-
-            if (GetWindowText(handle, Buff, nChars) > 0)
-            {
-                return Buff.ToString();
-            }
-            return null;
+            return GetActiveProcess().MainWindowTitle;
         }
 
         /**
@@ -39,11 +30,19 @@ namespace PolestarTracker.Core
          */
         public static string GetActiveProcessName()
         {
+            return GetActiveProcess().ProcessName;
+        }
+
+        /**
+         * Adapted from: https://stackoverflow.com/questions/17345202/get-the-current-active-application-name
+         */
+        public static Process GetActiveProcess()
+        {
             IntPtr windowHandle = GetForegroundWindow();
             uint pid = 0;
             GetWindowThreadProcessId(windowHandle, out pid);
             Process proc = Process.GetProcessById((int) pid);
-            return proc.ProcessName;
+            return proc;
         }
     }
 }
