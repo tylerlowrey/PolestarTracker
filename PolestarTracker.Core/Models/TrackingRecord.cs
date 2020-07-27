21133584332fd01aaf8f61459c27e7a828916288
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 16:21:59 -0400

    Added primary key annotation to TrackingRecord and generated migration

--- PolestarTracker.Core/Models/TrackingRecord.cs
+++ PolestarTracker.Core/Models/TrackingRecord.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PolestarTracker.Core.Models
 {
     public class TrackingRecord
     {
+        [Key]
         public int RecordId { get; set; }
         public string ProcessName { get; set; }
         public DateTime Timestamp { get; set; }
