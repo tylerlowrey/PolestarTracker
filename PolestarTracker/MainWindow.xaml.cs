812820f310f957ffb939f7834012a4c2a8af3dfe
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 13:48:44 -0400

    Finished and tested code for getting active window title

--- PolestarTracker/MainWindow.xaml.cs
+++ PolestarTracker/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using PolestarTracker.Core;
 
 namespace PolestarTracker
 {
@@ -23,6 +13,17 @@ namespace PolestarTracker
         public MainWindow()
         {
             InitializeComponent();
+            CurrentWindowTitle.Content = WindowTracker.GetActiveWindowTitle();
+
+            //Periodically update the currently active window title
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(500)
+            };
+
+            timer.Tick += (o, e) => { CurrentWindowTitle.Content = WindowTracker.GetActiveWindowTitle(); };
+            timer.IsEnabled = true;
+
         }
     }
 }
