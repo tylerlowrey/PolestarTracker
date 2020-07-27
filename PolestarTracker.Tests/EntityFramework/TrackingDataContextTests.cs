937cade911e05da558c429302b4c8cec511cfd94
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-27 16:42:53 -0400

    Refactored Tests project structure, added test skeleton for TrackingDataContext

---
+++ PolestarTracker.Tests/EntityFramework/TrackingDataContextTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PolestarTracker.EntityFramework;
+
+namespace PolestarTracker.Tests.EntityFramework
+{
+    [TestClass]
+    class TrackingDataContextTests
+    {
+    }
+}
