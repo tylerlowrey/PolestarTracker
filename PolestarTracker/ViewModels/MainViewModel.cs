8896349bd5c31b522c4dae72890964848a8f0b2b
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-29 14:58:56 -0400

    Added code for displaying daily application usage statistics

--- PolestarTracker/ViewModels/MainViewModel.cs
+++ PolestarTracker/ViewModels/MainViewModel.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
 using System.Windows.Threading;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
 using PolestarTracker.Core;
+using PolestarTracker.Core.Models;
+using PolestarTracker.EntityFramework;
 
 namespace PolestarTracker.WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase, INotifyPropertyChanged
     {
+        public SeriesCollection DailyApplicationUseDataCollection
+        {
+            get => _dailyApplicationUseDataCollection;
+            private set
+            {
+                _dailyApplicationUseDataCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SeriesCollection DailyProductivityDataCollection => _dailyProductivityDataCollection;
+
         public ICommand UpdateViewCommand { get; set; }
 
-        private string _currentWindowTitle;
-        private string _currentProcessName;
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private string _currentWindowTitle;
         public string CurrentWindowTitle
         {
             get => _currentWindowTitle;
@@ -29,6 +47,7 @@ namespace PolestarTracker.WPF.ViewModels
 
         }
 
+        private string _currentProcessName;
         public string CurrentProcessName
         {
             get => _currentProcessName;
@@ -38,9 +57,31 @@ namespace PolestarTracker.WPF.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        private TrackingDataContext _dbContext;
+        private SeriesCollection _dailyApplicationUseDataCollection;
+        private readonly SeriesCollection _dailyProductivityDataCollection;
+
         public MainViewModel(ViewNavigator navigator) : base(navigator)
         {
             UpdateViewCommand = new UpdateViewCommand(Navigator);
+
+            _dbContext = new TrackingDataContext();
+
+            DailyApplicationUseDataCollection = new SeriesCollection();
+            _dailyProductivityDataCollection = new SeriesCollection();
+
+            DispatcherTimer dataUpdateTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(60000),
+            };
+
+            dataUpdateTimer.Tick += (o, e) =>
+            {
+                UpdateChartData();
+            };
+            dataUpdateTimer.IsEnabled = true;
+
             CurrentWindowTitle = ProcessTracker.GetActiveWindowTitle();
             CurrentProcessName = ProcessTracker.GetActiveProcessName();
 
@@ -57,6 +98,41 @@ namespace PolestarTracker.WPF.ViewModels
             };
             timer.IsEnabled = true;
 
+            UpdateChartData();
+        }
+
+        private IEnumerable<TrackingRecord> GetTrackingData()
+        {
+            return _dbContext.TrackingRecords.AsEnumerable().ToList();
+        }
+
+        private void UpdateChartData()
+        {
+            var updatedDailyApplicationData = new SeriesCollection();
+            var listOfData = GetTrackingData();
+            var listOfActiveData = listOfData.Where(item => item.Active && item.Timestamp > DateTime.Today)
+                                             .ToList();
+
+            //Find all unique processes
+            var uniqueProcessesList = listOfActiveData.Select(item => item.ProcessName)
+                                                      .Distinct().ToList();
+            
+            uniqueProcessesList.ForEach(processName =>
+            {
+                updatedDailyApplicationData.Add(new PieSeries
+                {
+                    Title = processName,
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(listOfActiveData
+                                            .Select(item => item.ProcessName)
+                                            .Count(itemProcessName => itemProcessName == processName) / 100.0)
+                    },
+                    DataLabels = true,
+                });
+            });
+
+            DailyApplicationUseDataCollection = updatedDailyApplicationData;
         }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
