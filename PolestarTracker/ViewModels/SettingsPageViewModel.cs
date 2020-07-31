db6eab28ae63bc880cfa106a472156e8db4a427a
Tyler Lowrey <contact@tylerlowrey.com>
2020-07-31 14:08:05 -0400

    Added UI elements for settings page application aliases

--- PolestarTracker/ViewModels/SettingsPageViewModel.cs
+++ PolestarTracker/ViewModels/SettingsPageViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
+using PolestarTracker.Core.Models;
 
 namespace PolestarTracker.WPF.ViewModels
 {
     class SettingsPageViewModel : ViewModelBase
     {
         public ICommand UpdateViewCommand { get; set; }
+
+        public ObservableCollection<ApplicationAlias> ApplicationAliasesCollection { get; set; }
+        public ObservableCollection<ApplicationFilter> ApplicationFiltersCollection { get; set; }
         public SettingsPageViewModel(ViewNavigator navigator) : base(navigator)
         {
             UpdateViewCommand = new UpdateViewCommand(Navigator);
